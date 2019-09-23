53d17ae8c4d093ae5815d3a2c9aa8d696f3ef01a
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 20:11:58 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan15-Logika/src/IF0110118039Latihan15Logika.java
@@ -0,0 +1,22 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class IF0110118039Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + (!a));
+        System.out.println("!(a && b) = "+!(a && b));
+    }
+
+}
